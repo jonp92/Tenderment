b58393fe1d50e5554dd414718bcc337b8147ae99
jonathan@pressler.tech <jonathan@pressler.tech>
2023-03-01 17:02:59 +0000

    Created Homepage.ItemTemplate2 and Homepage.PrintSummary; Edited Homepage and settings; Deleted Homepage.ItemTemplate1

--- client_code/Homepage/PrintSummary/__init__.py
+++ client_code/Homepage/PrintSummary/__init__.py
@@ -1,16 +1,17 @@
-from ._anvil_designer import ItemTemplate1Template
+from ._anvil_designer import PrintSummaryTemplate
 from anvil import *
 import anvil.users
 import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from . import Homepage
 
-class ItemTemplate1(ItemTemplate1Template):
+class PrintSummary(PrintSummaryTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    RepeatingPanel().items = app_tables.images.search()
-    print(app_tables.images.search("*"))
+    self.label5.text = self.item['name']
+    
     # Any code you write here will run before the form opens.
 
--- client_code/Homepage/__init__.py
+++ client_code/Homepage/__init__.py
@@ -5,19 +5,24 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from . import PrintSummary
 
 class Homepage(HomepageTemplate):
   def __init__(self, **properties):
+    for row in app_tables.images.search():
+      self.print = {'name': row['name'], 'uploaded': row['uploaded'], 'weight': row['weight'], 'time': row['time'], 'price': row['price']}
+    self.repeating_panel_1.items = self.print
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-  
+
     # Any code you write here will run before the form opens.
   
-  def open_tickets(self):
+  def open_prints(self):
     self.ticket_form_open = True
+    self.current_form = ItemTemplate1
+    self.add_component(self.current_form, slot="default")
     
 
-  
   def link_1_click(self, **event_args):
     """This method is called when the link is clicked"""
     pass
