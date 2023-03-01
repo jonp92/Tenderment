bab35392c16542ceb2cb18f94463720b62a7c456
jonathan@pressler.tech <jonathan@pressler.tech>
2023-03-01 19:25:25 +0000

    Created user_check; Edited Homepage and Homepage.PrintSummary; Deleted Homepage.ItemTemplate1 and Login

--- client_code/Homepage/PrintSummary/__init__.py
+++ client_code/Homepage/PrintSummary/__init__.py
@@ -1,7 +1,7 @@
 from ._anvil_designer import PrintSummaryTemplate
 from anvil import *
-import anvil.users
 import anvil.server
+import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
@@ -10,8 +10,5 @@ class PrintSummary(PrintSummaryTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    print(self.item['name'])
-    self.filename.text = self.item['name']
-    
-    # Any code you write here will run before the form opens.
 
+    # Any code you write here will run before the form opens.
--- client_code/Homepage/__init__.py
+++ client_code/Homepage/__init__.py
@@ -9,10 +9,15 @@ from .PrintSummary import PrintSummary
 
 class Homepage(HomepageTemplate):
   def __init__(self, **properties):
-    self.repeating_panel_1.item_template = PrintSummary
-    self.repeating_panel = RepeatingPanel()
-    self.repeating_panel.items = app_tables.images.search()
-    print(f"self.item = {self.item}")
+    user = anvil.users.login_with_form()
+    authenticated = anvil.server.call('is_authenticated')
+    self.user = anvil.users.get_user()
+    if authenticated is True:
+      self.repeating_panel_1.item_template = PrintSummary
+      self.repeating_panel = RepeatingPanel()
+      self.repeating_panel_1.items = app_tables.images.search()
+    else:
+      pass
 
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
@@ -27,10 +32,25 @@ class Homepage(HomepageTemplate):
 
   def link_1_click(self, **event_args):
     """This method is called when the link is clicked"""
+    anvil.users.configure_account_with_form()
     pass
 
   def link_prints_click(self, **event_args):
     """This method is called when the link is clicked"""
     self.open_prints()
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.users.logout()
+    open_form()
+
+  def link_logoff_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    anvil.users.logout()
+    anvil.users.login_with_form()
+
+
+
+
 
 
