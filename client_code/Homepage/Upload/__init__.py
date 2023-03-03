c3aeaee6b17fcc18e94a7fc01b771bd381b3036e
jonathan@pressler.tech <jonathan@pressler.tech>
2023-03-03 02:21:38 +0000

    Created no_save.png; Edited settings, Homepage.Prints, png_processor and Homepage.Upload

--- client_code/Homepage/Upload/__init__.py
+++ client_code/Homepage/Upload/__init__.py
@@ -9,6 +9,8 @@ from anvil.tables import app_tables
 class Upload(UploadTemplate):
   def __init__(self, **properties):
     self.uploaded_file = 'None'
+    tcicon = anvil.server.get_app_origin() + "/_/theme/tcicon.png"
+    self.image_1.source = tcicon
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
@@ -16,9 +18,9 @@ class Upload(UploadTemplate):
 
   def file_loader_1_change(self, file, **event_args):
     """This method is called when a new file is loaded into this FileLoader"""
-    self.png = anvil.server.call('find_png', file, file.name)
+    self.png, uploaded_file = anvil.server.call('find_png', file, file.name)
     self.image_1.source = self.png
-    self.filename = file.name
+    self.filename = uploaded_file
     self.uploaded_file = file.name
     self.refresh_data_bindings()
 
--- server_code/png_processor.py
+++ server_code/png_processor.py
@@ -8,36 +8,41 @@ import anvil.media
 from re import search, DOTALL, sub, MULTILINE
 from base64 import b64decode
 from .Homepage import Homepage
+from .Homepage import Upload
 import datetime
-from decimal import Decimal
 now = datetime.datetime.now()
 date_string = now.strftime("%m_%d_%Y-%H:%M:%S")
+no_save = anvil.server.get_app_origin() + "/_/theme/no_save.png"
+
 @anvil.server.callable
 def find_png(media_object, uploaded_file):
-  with anvil.media.TempFile(media_object) as filename:
-    with open(filename, 'r') as f:
-      data = f.read()
-    pattern = r'; thumbnail begin 500x500 \d{5,}(.+); thumbnail end'
-    match = search(pattern, data, DOTALL)
-    png_table = None
-    if match:
-        # logging.info('Thumbnail data found and copied, now to strip and decode using base64')
-        thumbnail_data = match.group(1).strip()
-        thumbnail_data = sub(r'^; ', '', thumbnail_data, flags=MULTILINE)
-        thumbnail_data = b64decode(thumbnail_data)
-        return_media = anvil.BlobMedia(content_type="image/png", content=thumbnail_data)
-        match = search(r"\$(\d+\.\d+)-(\d+\.\d+)g-(.*).gcode", uploaded_file)
-        if match:
-          price = match.group(1)
-          weight = float(match.group(2))
-          time = match.group(3)
-        else:
-          price = 'Not Found'
-          weight = 'Not Found'
-          time = 'Not Found'
-        if uploaded_file not in [r['name'] for r in app_tables.prints.search()]:
-          png_table = app_tables.prints.add_row(name=uploaded_file, media_object=return_media, uploaded=date_string, price=price, weight=weight, time=time, status='New')
-    return return_media
+  if uploaded_file not in [r['name'] for r in app_tables.prints.search()]:
+    with anvil.media.TempFile(media_object) as filename:
+      with open(filename, 'r') as f:
+        data = f.read()
+      pattern = r'; thumbnail begin 500x500 \d{5,}(.+); thumbnail end'
+      match = search(pattern, data, DOTALL)
+      png_table = None
+      if match:
+          # logging.info('Thumbnail data found and copied, now to strip and decode using base64')
+          thumbnail_data = match.group(1).strip()
+          thumbnail_data = sub(r'^; ', '', thumbnail_data, flags=MULTILINE)
+          thumbnail_data = b64decode(thumbnail_data)
+          return_media = anvil.BlobMedia(content_type="image/png", content=thumbnail_data)
+          match = search(r"\$(\d+\.\d+)-(\d+\.\d+)g-(\d+)m.gcode", uploaded_file)
+          if match:
+            cost = float(match.group(1))
+            weight = float(match.group(2))
+            time = float(match.group(3))
+          else:
+            price = 'Not Found'
+            weight = 'Not Found'
+            time = 'Not Found'
+          png_table = app_tables.prints.add_row(name=uploaded_file, media_object=return_media, uploaded=date_string, cost=cost, weight=weight, time=time, status='New')      
+      return return_media, uploaded_file
+  else:
+    return no_save
+  
 
 @anvil.server.callable
 def find_png_gcode(data, filename):
