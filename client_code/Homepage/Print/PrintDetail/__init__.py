f4374f5e2ffd3cf5753d6b7425927a2745a5821e
jonathan@pressler.tech <jonathan@pressler.tech>
2023-03-05 15:44:16 +0000

    Edited Homepage.Print.PrintSummary and Homepage.Print.PrintDetail

--- client_code/Homepage/Print/PrintDetail/__init__.py
+++ client_code/Homepage/Print/PrintDetail/__init__.py
@@ -8,16 +8,14 @@ from anvil.tables import app_tables
 
 class PrintDetail(PrintDetailTemplate):
   def __init__(self, item_id, **properties):
-    self.name = None
     self.item = anvil.server.call('get_print_by_id', item_id)
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+  def update_print_data(self):
+    anvil.server.call('update_print_data', self.item['id'], self.item['name'], self.item['uploaded'], self.item['time'], self.item['cost'], self.item['weight'], self.item['status'])
 
-  def text_box_1_lost_focus(self, **event_args):
-    """This method is called when the TextBox loses focus"""
-    self.name = self.text_box_1.text
 
 
 
