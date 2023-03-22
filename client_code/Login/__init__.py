0efbdd7855417fddcf9fdd109031822a5ed809f2
jonathan@pressler.tech <jonathan@pressler.tech>
2023-03-22 16:03:18 +0000

    Edited Login

--- client_code/Login/__init__.py
+++ client_code/Login/__init__.py
@@ -10,11 +10,16 @@ class Login(LoginTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
     # Any code you write here will run before the form opens.
 
   def button_login_click(self, **event_args):
     """This method is called when the button is clicked"""
     anvil.users.login_with_form()
     open_form('Homepage')
+
+  def form_show(self, **event_args):
+    """This method is called when the column panel is shown on the screen"""
+    if anvil.users.get_user():
+      open_form('Homepage')
+
 
