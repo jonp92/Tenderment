ab36ae5393af4ec8f16673e11cdda85d6917480c
jonathan@pressler.tech <jonathan@pressler.tech>
2023-03-23 21:19:23 +0000

    Edited uploader and Homepage.Upload

--- server_code/uploader.py
+++ server_code/uploader.py
@@ -5,11 +5,19 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
 import shortuuid
+import re
 
+def name_sanitizer(filename):
+  regex = r"[()\s_]+(?!\.)"
+  replacement = "-"
+  return re.sub(regex, replacement, filename)
+  
 @anvil.server.callable
 def upload_stl(file, filename):
+  filename = name_sanitizer(filename)
   existing_row = app_tables.stls.get(name=filename)
   if not existing_row:
     app_tables.stls.add_row(name=filename, stl=file, id=shortuuid.uuid())
+    return filename
   else:
     raise Exception('Filename already exists, please rename the file before uploading')
