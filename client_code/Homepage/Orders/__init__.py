89cc20ccb9c48523bc1f458e8037efbc466f6ae8
jonathan@pressler.tech <jonathan@pressler.tech>
2023-03-27 19:04:16 +0000

    Edited Homepage.Orders and Homepage.TableExplorer

--- client_code/Homepage/Orders/__init__.py
+++ client_code/Homepage/Orders/__init__.py
@@ -5,7 +5,7 @@ import anvil.server
 class Orders(OrdersTemplate):
   def __init__(self, **properties):
     url = f"/commerce/orders"
-    self.order = anvil.server.call('get_sqdata', url)
+    #self.order = anvil.server.call('get_sqdata', url)
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
