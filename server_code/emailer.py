ddf195ab51f6dcc1a6ebe1b944cbc66b59a43b4c
jonathan@pressler.tech <jonathan@pressler.tech>
2023-03-28 20:18:05 +0000

    Edited emailer

--- server_code/emailer.py
+++ server_code/emailer.py
@@ -9,7 +9,7 @@ import anvil.server
 @anvil.server.callable
 def test_email_send():
   anvil.email.send(from_name = "Tenderment",
-                 from_address = "support@badjholdings.com",  
+                 from_address = "support@tenderment.com",  
                  to = "jonathan@pressler.tech",
                  subject = "Test",
                  text = "This is a successful test of the Tenderment outbound email service.")
