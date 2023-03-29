94cc78359bede5a8573578aabb6ea877a27bb7d6
jonathan@pressler.tech <jonathan@pressler.tech>
2023-03-29 23:24:05 +0000

    Edited Homepage.Settings and Homepage.Settings.LogTail

--- client_code/Homepage/Settings/__init__.py
+++ client_code/Homepage/Settings/__init__.py
@@ -45,7 +45,7 @@ class Settings(SettingsTemplate):
 
   def button_tail_anvil_log_click(self, **event_args):
     """This method is called when the button is clicked"""
-    alert(LogTail(self.text_box_lines_tail.text), title='Tail of Tenderment Anvil Log', large=True)
+    alert(LogTail(self.text_box_lines_tail.text, self.drop_down_log_selected.selected_value), title='Tail of Tenderment Anvil Log', large=True)
 
 
 
