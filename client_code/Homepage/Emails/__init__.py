c16c92494ec419ab7daab3e60d02b6466ed5f4f5
jonathan@pressler.tech <jonathan@pressler.tech>
2023-03-30 01:28:53 +0000

    Edited Homepage.Emails

--- client_code/Homepage/Emails/__init__.py
+++ client_code/Homepage/Emails/__init__.py
@@ -8,13 +8,16 @@ from anvil.tables import app_tables
 
 class Emails(EmailsTemplate):
   def __init__(self, **properties):
-    email_data = anvil.server.call('get_email_data')
+    self.get_email_data()
     self.item = email_data.search()
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
 
+  def get_email_data(self, **event_args):
+    email_data = anvil.server.call('get_email_data')
+  
   def delete_click(self, **event_args):
     """This method is called when the button is clicked"""
     # Step through each template form (row) in the repeating panel.
@@ -24,6 +27,7 @@ class Emails(EmailsTemplate):
         if type(c) is CheckBox:
           if c.checked == True:
             r.item.delete()
-    self.refresh_data_bindings()
+  self.get_email_data()
+    
 
 
