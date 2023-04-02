f2ae80ae168ad7e92042078a6c2a0c2612caa733
jonathan@pressler.tech <jonathan@pressler.tech>
2023-04-02 01:09:23 +0000

    Created interfaces; Edited settings

---
+++ server_code/interfaces.py
@@ -0,0 +1,11 @@
+import anvil.email
+import anvil.secrets
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+
+@anvil.server.background_task
+def update_services_status():
+  anvil.server.call('get_services_status')
