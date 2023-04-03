1166055516f756d986e19c66125ec334f39e1f22
jonathan@pressler.tech <jonathan@pressler.tech>
2023-04-03 14:14:51 +0000

    Edited downloader

--- server_code/downloader.py
+++ server_code/downloader.py
@@ -4,7 +4,14 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
 
-@anvil.server.callable
+def authorization():
+  if anvil.users.get_user():
+    if anvil.users.get_user()['role'] == 'admin' or 'superadmin':
+      return True
+    else:
+      return False
+
+@anvil.server.callable(require_user=authorization())
 def get_gcode_download(id):
   print_row = app_tables.prints.get_by_id(id)
   if not stl_row:
@@ -12,7 +19,7 @@ def get_gcode_download(id):
   else:  
     return anvil.BlobMedia(content_type="application/octet-stream", content=print_row['gcode'].get_bytes(), name=f"{print_row['name']}.gcode")
 
-@anvil.server.callable
+@anvil.server.callable(require_user=authorization())
 def get_stl_download(print_row):
   stl_row = app_tables.stls.get(print=print_row)
   if not stl_row:
@@ -20,7 +27,7 @@ def get_stl_download(print_row):
   else:
    return anvil.BlobMedia(content_type="application/octet-stream", content=stl_row['stl'].get_bytes(), name=f"{stl_row['name']}")
 
-@anvil.server.callable
+@anvil.server.callable(require_user=authorization())
 def get_email_attachments(email_row):
   attachments_row = app_tables.attachments.get(message=email_row)
   if not attachments_row:
