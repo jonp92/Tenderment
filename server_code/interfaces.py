bf95c8ce861d982a2d44b7dae51c1ffba9edb629
jonathan@pressler.tech <jonathan@pressler.tech>
2023-04-03 17:15:08 +0000

    Edited interfaces and settings

--- server_code/interfaces.py
+++ server_code/interfaces.py
@@ -6,6 +6,8 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
 
-#@anvil.server.background_task
-#def update_services_status():
-  #anvil.server.call('get_services_status')
+@anvil.server.background_task
+def get_printer_status():
+  printer_power_status = anvil.server.call('get_printer_power_status')
+  status_row = app_tables.services_status.get()
+  status_row.update(printer_power_status=printer_power_status)
